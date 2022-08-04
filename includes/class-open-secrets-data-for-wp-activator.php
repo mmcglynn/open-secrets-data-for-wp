9df0c7de23b812ecf560e62460719f6f35580eaa
Michael McGlynn <mike@reignition.net>
2022-08-04 18:06:58 -0400

    Initial commit

---
+++ includes/class-open-secrets-data-for-wp-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.reignitionllc.com/
+ * @since      1.0.0
+ *
+ * @package    Open_Secrets_Data_For_Wp
+ * @subpackage Open_Secrets_Data_For_Wp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Open_Secrets_Data_For_Wp
+ * @subpackage Open_Secrets_Data_For_Wp/includes
+ * @author     Michael McGlynn <mike@reignition.net>
+ */
+class Open_Secrets_Data_For_Wp_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-open-secrets-data-for-wp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.reignitionllc.com/
+ * @since      1.0.0
+ *
+ * @package    Open_Secrets_Data_For_Wp
+ * @subpackage Open_Secrets_Data_For_Wp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Open_Secrets_Data_For_Wp
+ * @subpackage Open_Secrets_Data_For_Wp/includes
+ * @author     Michael McGlynn <mike@reignition.net>
+ */
+class Open_Secrets_Data_For_Wp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
